f094388f878456a825bcf8f5dd354fec0e66c4ba
Sobirjonov Sanjarbek <51774058+sobirjonovs@users.noreply.github.com>
2021-07-20 07:07:07 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,11 @@
 require_once "vendor/autoload.php";
 
 use Handlers\Api;
-use Payment\IpakYuli;
 use Handlers\Handler;
 use Telegram\Bot\Exceptions\TelegramSDKException;
 
 try {
     $telegram = new Api(config('bot.token'));
-    $telegram->payment = new IpakYuli();
 
     $handler = new Handler($telegram);
     $handler->map(['User']);
